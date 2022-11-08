64653852aba70513e5b2d9430e4f89231e0e0115
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-08 16:36:12 +0100

    button

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const nome = document.getElementById("output");
+const km = document.getElementById("chilometri");
+const button = document.querySelector ("button")
+
+console.log(nome,chilometri);
+
